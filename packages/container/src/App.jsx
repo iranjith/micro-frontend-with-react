3e2e0a9b71dc8b4de7af40c21567e0a77b4f5cf2
RANJITH KUMAR <theranjith@gmail.com>
2026-04-09 07:16:33 +0530

    running microfrontend

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -2,9 +2,34 @@ import React from "react";
 import "./app.css";
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <>
+      <HomePage />
+      <RemoteProducts />
+      <RemoteCart />    
+    </>
+    
+  );
 }
 
 function HomePage() {
   return <div>Welcome to our micro-frontend store!</div>;
+}
+
+function RemoteProducts() {
+  const ProductList = React.lazy(() => import("products/Products"));
+  return (
+    <React.Suspense fallback="Loading products...">
+      <ProductList />
+    </React.Suspense>
+  );
+}
+
+function RemoteCart() {
+  const Cart = React.lazy(() => import("cart/Cart"));
+  return (
+    <React.Suspense fallback="Loading cart...">
+      <Cart />
+    </React.Suspense>
+  );
 }
